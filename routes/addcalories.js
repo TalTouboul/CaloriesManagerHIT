e771575b95d12706c6cf5e81acc6ec74718ab9a6
TalTouboul <109070558+TalTouboul@users.noreply.github.com>
2024-06-30 20:35:02 +0300

    Update addcalories.js

--- routes/addcalories.js
+++ routes/addcalories.js
@@ -6,6 +6,7 @@ Tal Tubul - 208835355
 const express = require('express'); // Importing the Express framework
 const router = express.Router(); // Creating an instance of an Express router
 const Calories = require('../models/calories'); // Importing the Calories model
+const { addNewCalorieRecord } = require('../controllers/calories'); // Importing the addNewCalorieRecord function from the calories controller
 
 // Fetch all calorie records
 router.get('/', async function(req, res, next) {
@@ -18,7 +19,7 @@ router.get('/', async function(req, res, next) {
 }); 
 
 // Create a new calorie record
-router.post("/addcalories", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
         const calories = await addNewCalorieRecord(req.body); // Calling the function to add a new calorie record
         res.send(calories); // Sending the newly created calorie record as a response
